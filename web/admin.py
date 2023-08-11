0432ed997d00ebfd974cd506f0564f5097bcd245
SeoYun <rockin99kr@gmail.com>
2023-08-11 22:58:19 +0900

    0811 유저 로그인/카테고리 뷰 수정2

--- web/admin.py
+++ web/admin.py
@@ -4,13 +4,7 @@ from django.contrib.auth.forms import UserChangeForm
 
 from .models import User
 
-# class CustomUserUpdateForm(UserChangeForm):
-#     class Meta:
-#         model = User
-#         fields = '__all__'
-
 class CustomUserAdmin(UserAdmin):
-    # form = CustomUserUpdateForm
     list_display = ['username', 'id', 'category', 'is_active']
 
 admin.site.register(User, CustomUserAdmin)
